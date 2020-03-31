a78c614a974fc0366213de1b02a12ff635eb7017
zbarni97 <59418443+zbarni97@users.noreply.github.com>
2020-03-31 19:06:16 +0200

    money_input and pocket print

--- roulette.py
+++ roulette.py
@@ -1,13 +1,25 @@
 #  Here comes the code!
 #  May the force be with you, young padawan!
-
+import os
 
 def money_input():
-    pass
+    money = 0
+    value = input("Mennyi pénzzel szeretnéd kezdeni? \n 1. 1000 \n 2. 5000 \n 3. 10000 \n")
+
+    if value == '1':
+        money = 1000
+    if value == '2':
+        money = 5000
+    if value == '3':
+        money = 10000
+   
+    return money
 
 
-def pocket():
-    pass
+def pocket(money):
+    print(money)
+
+    
 
 
 def bet_input():  # $$
@@ -31,7 +43,11 @@ def is_winner():
 
 
 def main():
-    pass
+    money = money_input()
+    os.system('clear')
+    print('Money in your pocket: ')
+    pocket(money)
+    
 
 
 if __name__ == "__main__":
